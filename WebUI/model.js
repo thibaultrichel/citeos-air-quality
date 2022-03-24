35cb6122ab972a96994bc3fb6396a4e9de004cb5
thibzzz09 <thibault.richel2@gmail.com>
2022-03-24 17:48:12 +0100

    testing tfjs model

--- WebUI/model.js
+++ WebUI/model.js
@@ -1,8 +1,16 @@
 const tf = require("@tensorflow/tfjs");
 const csv = require("jquery-csv");
 
-const model = await tf.loadLayersModel("model_utils/LSTM_multi_with_target.h5");
-let data = csv.toObjects("model_utils/merged-final.csv");
-let size = data.size;
+async function loadModel() {
+    return await tf.loadLayersModel("/Users/thibaultrichel/Desktop/JupyterLab/ProjetCITEOS/WebUI/model_utils/tfjsmodel/model.json");
+}
 
-module.exports = {size};
+let model = loadModel();
+// let data = csv.toObjects("model_utils/merged-final.csv");
+// let size = data.size;
+//
+// module.exports = {size};
+//
+// console.log(size);
+
+console.log(model);
